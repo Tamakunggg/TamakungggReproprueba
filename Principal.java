5f6eb8d011f3e53f4c9ce363054cb3325fae68d0
Tamakunggg <Alvaropcx7@gmail.com>
2024-04-28 18:03:10 -0700

    Se añadio el archivo principal.java

---
+++ Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        int num1 =2, num2 =3;
+        if (num1 < num2) {
+            System.err.println("EL numero 1 es menor");
+        }else if (num2 < num1) {
+            System.err.println("EL numero 2 es menor");
+        }else{
+            System.err.println("Los numeros son iguales");
+        }
+    }
+    
+}
